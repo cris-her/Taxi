4d6a760c263d7c56220e651a7c5156341886185d
CrisFiss <crs.hdz.s@gmail.com>
2021-04-29 19:51:55 -0500

    added ModifiedUsers migration

---
+++ Taxi.Web/Migrations/20210430004948_ModifiedUsers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Taxi.Web.Migrations
+{
+    public partial class ModifiedUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "TripDetails",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "TripDetails");
+        }
+    }
+}
